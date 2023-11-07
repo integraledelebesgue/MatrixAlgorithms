42ea820a6be5cc2323f3ec2002c1995a1bb0c73a
integraledelebesgue <jsmolka@student.agh.edu.pl>
2023-11-07 19:46:44 +0100

    Optimization

--- Lab2/src/algorithms/Determinant.jl
+++ Lab2/src/algorithms/Determinant.jl
@@ -1,56 +1,77 @@
 module Determinant
 
-using LinearAlgebra: SingularException
 using LoopVectorization: @turbo
 using Base: @assume_effects
+using LinearAlgebra: similar
 
 function swap_rows!(matrix::Matrix{Float64}, i::Int, j::Int)::Nothing
-    @turbo @fastmath matrix[i, :], matrix[j, :] = matrix[j, :], matrix[i, :]
+    n = size(matrix, 1)
+
+    @inbounds @simd for col in 1:n
+        tmp = matrix[i, col]
+        matrix[i, col] = matrix[j, col]
+        matrix[j, col] = tmp
+    end
+
     nothing
 end
 
 function get_pivot(matrix::Matrix{Float64}, col::Int)::Tuple{Int, Float64}
-    @inbounds begin
-        relative_row = @view(matrix[col:end, col]) .|> abs |> argmax
-        row = relative_row + col - 1
-        pivot = matrix[row, col]
+    n = size(matrix, 1)
+    @inbounds pivot = abs(matrix[col, col])
+    pivot_row = col
+
+    @simd for row in col+1:n 
+        @inbounds element_abs = abs(matrix[row, col])
+
+        if element_abs > pivot
+            pivot = element_abs
+            pivot_row = row
+        end
     end
-    
-    (row, pivot)
+
+    return (pivot_row, pivot)
 end
 
 function divide_row!(matrix::Matrix{Float64}, row::Int)::Nothing
-    @turbo @fastmath matrix[row, :] ./= matrix[row, row]
+    @inbounds inv_pivot = inv(matrix[row, row])
+    n = size(matrix, 1)
+
+    @simd for i in row+1:n
+        @inbounds @fastmath matrix[row, i] *= inv_pivot
+    end
+
     nothing
 end
 
-function reduce_column!(matrix::Matrix{Float64}, col::Int)::Nothing
+@polly function reduce_column!(matrix::Matrix{Float64}, col::Int)::Nothing
     n = size(matrix, 1)
 
-    @simd for row in col+1:n
-        @turbo matrix[row, :] .-= matrix[row, col] * matrix[col, :]
+    @inbounds for i in col+1:n
+        element = matrix[i, col]
+        @simd for j in 1:n
+            @fastmath matrix[i, j] -= element * matrix[col, i]
+        end
     end
 
     nothing
 end
 
 @assume_effects :total function det(matrix::Matrix{Float64})::Float64
-    n = size(matrix, 1)
     matrix = copy(matrix)
-    pivots = Vector{Float64}(undef, n)
-    pivots[end] = 1.0
+    n = size(matrix, 1)
+    n_rows_permuted = 0
+    result = 1.0
 
-    for row_col in 1:n-1
+    for row_col in 1:n
         pivot_row, pivot = get_pivot(matrix, row_col)
 
-        if pivot ≈ 0.0
-            return 0.0
-        end
+        pivot ≈ 0.0 && return 0.0
 
-        pivots[row_col] = pivot
+        @fastmath result *= pivot
 
         if pivot_row != row_col
-            pivots[end] *= -1.0
+            n_rows_permuted += 1
         end
 
         swap_rows!(matrix, row_col, pivot_row)
@@ -59,7 +80,9 @@ end
         reduce_column!(matrix, row_col)
     end
 
-    return @fastmath prod(pivots) * matrix[end, end]
+    sgn = isodd(n_rows_permuted) ? -1.0 : 1.0
+
+    return @fastmath result * sgn
 end
 
 end# module
